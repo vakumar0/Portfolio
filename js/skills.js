4163e5b80bec71231bfb2fc86afb9174e58cfa2a
Vaibhav Kumar <kumarvaibhav1602@gmail.com>
2020-03-29 22:04:53 +0530

    Added porlais.html, updated the index.html

--- js/skills.js
+++ js/skills.js
@@ -50,103 +50,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     var myChart = Highcharts.chart('skillCompareContainer', {
-            chart: {
-                type: 'lollipop',
-                backgroundColor: '#000000'
-            },
+        chart: {
+            type: 'lollipop',
+            backgroundColor: '#000000'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['Data Structures', 'JavaScript', 'NodeJS', 'C#', "MySQL","Elastic Search", "JQuery", "SQL Server", "HTML5","KnockoutJS","Restful API","CSS3"]
+        },
+        yAxis: {
             title: {
-                text: ''
-            },
-            xAxis: {
-                categories: ['Data Structures', 'JavaScript', 'NodeJS', 'C#', "MySQL","Elastic Search", "JQuery", "SQL Server", "HTML5","KnockoutJS","Restful API","CSS3"]
-            },
-            yAxis: {
-                title: {
-                    text: 'Skills'
-                }
-            },
-            credits: {
-                enabled: false
-            },
-            series: [{
-                data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-            }]
-        });
+                text: 'Skills'
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        }]
+    });
 
-    // var myChart = Highcharts.chart('skillsContent', {
-    //     chart: {
-    //         type: 'packedbubble',
-    //         height: '80%'
-    //     },
-    //     title: {
-    //         text: 'Simple packed bubble'
-    //     },
-    //     subTitle: {
-    //         text: 'Coffee consumption'
-    //     },
-    //     tooltip: {
-    //         useHTML: true,
-    //         pointFormat: '<b>{point.name}:</b> {point.y}</sub>'
-    //     },
-    //     plotOptions: {
-    //         packedbubble: {
-    //             dataLabels: {
-    //                 enabled: true,
-    //                 format: '{point.name}',
-    //                 style: {
-    //                     color: 'black',
-    //                     textOutline: 'none',
-    //                     fontWeight: 'normal'
-    //                 }
-    //             },
-    //             minPointSize: 5
-    //         }
-    //     },
-    //     series: [{
-    //         name: 'Coffee',
-    //         data: [{
-    //             value: 12, name: 'Bert'
-    //         }, {
-    //           value: 5, name: 'Sam'
-    //         }, {
-    //           value: 10, name: 'John'
-    //         }, {
-    //             value: 7, name: 'Dick' 
-    //         }]},{
-    //             name: 'Energy drinks',
-    //           data: [{
-    //              value: 10, name: 'Ma'
-    //           }]
-    //         }, {
-    //             name: 'Tea',
-    //           data: [5, 6, 8, {value: 10, name: 'Mustapha', color: 'pink'}]
-    //         }]
-    // });
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-    //     var myChart = Highcharts.chart('skillsContent', {
-    //         chart: {
-    //             type: 'bar'
-    //         },
-    //         title: {
-    //             text: ''
-    //         },
-    //         xAxis: {
-    //             categories: ['Data Structures', 'JavaScript', 'NodeJS', 'C#', "MySQL","Elastic Search", "JQuery", "SQL Server", "HTML5","KnockoutJS","Restful API","CSS3"]
-    //         },
-    //         yAxis: {
-    //             title: {
-    //                 text: 'Skills'
-    //             }
-    //         },
-    //         credits: {
-    //             enabled: false
-    //         },
-    //         series: [{
-    //             data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    //         }]
-    //     });
-    // });
 
 
